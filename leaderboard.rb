9f42f2a22e50bfbe40d93f33a789f8aa6c88637f
paraspace32 <paraspace3@gmail.com>
2020-10-07 18:02:58 +0530

    Create leaderboard.rb

---
+++ leaderboard.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# This class contains latest tally of players
+class Leaderboard
+  def create_tally(players, current_standing)
+    data = players.sort_by { |p| [p.rank, -p.points] }
+    previous_rank = current_standing - 1
+    puts 'Current tally'
+    data.each do |player|
+      current_rank = if player.rank > players.count
+                       previous_rank + 1
+                     else
+                       player.rank
+                     end
+      previous_rank = current_rank
+      puts "Player: #{player.name} Point: #{player.points} Rank: #{current_rank}"
+    end
+  end
+
+  def assign_rank(player, current_standing)
+    player.rank = current_standing
+    puts "#{player.name} won with rank: #{player.rank}"
+  end
+end
